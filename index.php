fbb7fc93fc7e063e8301d64f3ab4836219dabb65
drowninginsins <66271553+drowninginsins@users.noreply.github.com>
2020-06-01 19:59:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Home | Drowning In Sins</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="icon" href="images/blacklogo.png">
+</head>
+
+<body background="https://i.ytimg.com/vi/1iIXx55Orwg/maxresdefault.jpg">
+
+	 <a href="register.php"><button style="float: right; margin-right: 50px;" class="button" type="submit" form="nameform" value="Submit">Register</button></a>
+	 <a href="login.php"><button style="float: right; margin-right: 50px;" class="button" type="submit" form="nameform" value="Submit">Log In</button></a>
+	 
+ <center><img style="padding:20px" height="100px" src="images/dis_logo.png"> 
+     <img style="padding:20px" height="100px" src="images/dis_title.png"></center>
+	 
+	 
+<!--navigation-->
+	 <div class="navi">
+            <br>
+          <div class="box"><a href="index.php"><img src="/images/home.png"></a></div>
+          <div class="box"><a href="about.php"><img src="/images/about.png"></a></div>
+          <div class="box"><a href="store.php"><img src="/images/store.png"></a></div>
+            </div>
+      
+            
+     <div class="main">
+	 <h1> Welcome to the site </h1>
+	 
+</div>
+
+
+  </body>
+</html>
+
